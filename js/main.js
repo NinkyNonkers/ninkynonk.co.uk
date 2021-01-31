5376d5fc92d51b58f082dd7ac9496be71f06b1de
archie426 <archiestride42@gmail.com>
2021-01-31 13:07:01 +0000

    Lots of styling Fix colour animations, add space 404 page, create links for menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 let color;
+let colourStatus = new Object();
 
 
 function mouseOver(bgColor) {
+  colourStatus[bgColor] = true;
   color = bgColor;
   changeBgColor($("#pageWrapper"), bgColor)
 }
 
-function mouseLeave() {
+function mouseLeave(bgColor) {
+  colourStatus[bgColor] = false;
   color = "#BDB2FF";
-  changeBgColor($("#pageWrapper"), "#BDB2FF");
+  for (var color in colourStatus) {
+    if (colourStatus[color] == true)
+      return;
+  }
+  setTimeout(() => {
+    for (var color in colourStatus) {
+      if (colourStatus[color] == true)
+        return;
+    }
+    changeBgColor($("#pageWrapper"), "#BDB2FF");
+  }, 200);
 }
 
 function changeBgColor($, color) {
