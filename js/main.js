0774883114983424519e9ea2006f29d2a23d19e8
archie426 <archiestride42@gmail.com>
2021-02-01 13:08:15 +0000

    Prepare media page layout and meta

--- js/main.js
+++ js/main.js
@@ -1,35 +1,26 @@
-let color;
 let colourStatus = new Object();
 
 
 function mouseOver(bgColor) {
   colourStatus[bgColor] = true;
-  color = bgColor;
   changeBgColor($("#pageWrapper"), bgColor)
 }
 
 function mouseLeave(bgColor) {
   colourStatus[bgColor] = false;
-  color = "#BDB2FF";
-  for (var color in colourStatus) {
-    if (colourStatus[color] == true)
+  for (const color in colourStatus) {
+    if (colourStatus[color] === true)
       return;
   }
   setTimeout(() => {
-    for (var color in colourStatus) {
-      if (colourStatus[color] == true)
+    for (const color in colourStatus) {
+      if (colourStatus[color] === true)
         return;
     }
     changeBgColor($("#pageWrapper"), "#BDB2FF");
   }, 200);
 }
 
-function showComingSoonText(bgColor) {
-
-}
-
 function changeBgColor($, color) {
-  $.animate({backgroundColor: color}, {progress: function(animation, progress, msRemaining){
-
-    }});
+  $.animate({backgroundColor: color});
 }
---
+++ js/video.js
@@ -0,0 +1,15 @@
+let videos = ["media.mp4"];
+
+
+function setVideo(){
+  let videoWrapper = document.getElementById("video");
+  videoWrapper.setAttribute("src", "video/" + videos[Math.floor(Math.random() * slogans.length)])
+}
+
+function displayVideos() {
+  videos = document.getElementById();
+  for (const video in videos) {
+
+  }
+}
+
